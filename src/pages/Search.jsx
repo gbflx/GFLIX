c9f99aef6b1ab53f004df72321e90982a7f6ecf9
Gabriel Felix <pmvulgo@gmail.com>
2024-02-24 02:39:46 -0300

    pronto

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -5,22 +5,36 @@ import MovieCard from "../components/MovieCard";
 const SearchURL = import.meta.env.VITE_SEARCH;
 const apiKey = import.meta.env.VITE_API_KEY;
 
-
 import "./MovieGrid.css";
 
 const Search = () => {
+  const [searchParams] = useSearchParams();
+
+  const [movies, setMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    setMovies(data.results);
+  };
+
+  useEffect(() => {
+    const searchWithQueryURL = `${SearchURL}?${apiKey}&query=${query}`;
+    getSearchedMovies(searchWithQueryURL);
+  }, [query]);
+
   return (
     <div className="container">
       <h2 className="title">
         Resultados para: <span className="query-text">{query}</span>
       </h2>
       <div className="movies-container">
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 &&
-         topMovies.map((movie) => <MovieCard key={movie.id} movie={movie}/>)}
+        {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
-  </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Search
+export default Search;
